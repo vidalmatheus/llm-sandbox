b7b8c2adefb6c7c98d9663800784068a19b9aa20
Matheus Vidal <matheusvidaldemenezes@gmail.com>
2023-10-19 13:09:38 -0300

    Chatting with a legislation

--- langchan.py
+++ langchan.py
@@ -1,6 +1,5 @@
 import os
 import sys
-from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import TextLoader
 from langchain.indexes import VectorstoreIndexCreator
