6e858baa335b9e7dfc48f78853cf0b9471c7bd6e
Matheus Vidal <matheusvidaldemenezes@gmail.com>
2023-11-27 23:58:31 -0300

    Gen images with dall-e-3

--- chatgpt.py
+++ chatgpt.py
@@ -1,4 +1,5 @@
 import os
+import re
 import openai
 import requests
 from PIL import Image
@@ -29,16 +30,72 @@ def get_sentiment_from(text):
 # get_sentiment_from("I luv u")
 # get_sentiment_from("I'm pretty excited about it")
 
-def show_images_from_openai_response(image_resp):
-    for img in image_resp["data"]:
-        img_url = img["url"]
-        response = requests.get(img_url)
+def show_images_from_openai_response(image_url):
+    # Download the image
+    response = requests.get(image_url)
+
+    if response.status_code == 200:
+        # Read the image data
         image_data = BytesIO(response.content)
+
+        # Open the image using PIL
         image = Image.open(image_data)
+
+        # Display the image
         image.show()
 
-def gen_image(prompt):
-    image_resp = openai.Image.create(prompt=prompt, n=2, size="512x512")
-    show_images_from_openai_response(image_resp)
+        # Extract filename pattern "img-BdAsWGPtwIrVVIS9CUzeEM05.png"
+        filename_match = re.search(r'img-[A-Za-z0-9]+\.png', image_url)
+        if filename_match:
+            filename = filename_match.group()
+        else:
+            filename = "downloaded_image.png"
 
-gen_image("A happy person jumping off the christ the redeemer in Rio de Janeiro")
+        # Save the image in the current directory with the extracted filename
+        save_path = os.path.join(os.getcwd(), filename)
+        image.save(save_path)
+        print(f"Image saved as {save_path}")
+    else:
+        print(f"Failed to download image. Status code: {response.status_code}")
+
+
+def gen_image(prompt, model="dall-e-3"):
+    response = openai.images.generate(
+        model=model,
+        prompt=prompt,
+        size="1024x1024",
+        quality="hd",
+        n=1,
+    )
+
+    for img in response.data:
+        image_url = img.url
+        print(image_url)
+        show_images_from_openai_response(image_url)
+
+
+def gen_variant(prompt, model="dall-e-2"):
+    card = Image.new("RGBA", (512, 512), (255, 255, 255))
+    img = Image.open("me.png").convert("RGBA")
+    x, y = img.size
+    card.paste(img, (0, 0, x, y), img)
+    card.save("me-2.png", format="png")
+    response = openai.images.edit(
+        model=model,
+        image=open("me-2.png", "rb"),
+        mask=open("me-2.png", "rb"),
+        prompt=prompt,
+        n=1,
+        size="1024x1024"
+    )
+    
+    for img in response.data:
+        image_url = img.url
+        print(image_url)
+        show_images_from_openai_response(image_url)
+
+
+gen_image("A simple gold necklace with a 5 cm circular pendant of the trxee of life in a given person wearing it")
+# gen_image("A woman wearing in her finger a simple plate ring with a little rose gold paw")
+# gen_image("Uma pulseira de prata de berloques da Taylor Swift sendo um do album 1989, um do album reputation, um do album speak now, um do album folklore, um do album lover, um do album midnight")
+
