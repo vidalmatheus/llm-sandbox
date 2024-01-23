a2b651d9914bdc5788fbd3baa45785b2fd31a127
Matheus Vidal <matheusvidaldemenezes@gmail.com>
2024-01-22 23:42:57 -0300

    Checking intonation

--- voice.py
+++ voice.py
@@ -9,9 +9,10 @@ response = client.audio.speech.create(
     model="tts-1",
     voice="alloy",
     input="""
-		Oláaaaa, meu caro! Fala tu, como vai?
-		Acabei de receber uma ligação sua daqui e
-		queria entender o que realmente procura, tudo bem para você?"
+		Oláaaaa, meu caro! Eaí, como vai seu dia?
+		Espero que bem em. Então... eu estava pensando naquela
+		contraproposta que nos pediu e tenho uma ótima notícia.
+		Vimos que é possível ser ainda 20% mais barato!!!
 	"""
 )
 
